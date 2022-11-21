3e91a4706eba817efb81f6709f19969ab79fe924
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2022-11-21 03:29:45 -0800

    made changes

--- blackjack.js
+++ blackjack.js
@@ -19,12 +19,15 @@ const getDeck = () => {
           break;
         case j == 11:
           valName = "Jack";
+          val = 10;
           break;
         case j == 12:
           valName = "Queen";
+          val = 10;
           break;
         case j == 13:
           valName = "King";
+          val = 10;
           break;
         default:
           break;
@@ -79,15 +82,37 @@ const player = new CardPlayer("Player");
  */
 const calcPoints = (hand) => {
   // CREATE FUNCTION HERE
-  let total = 0;
-  let isSoft = false;
+  let handTotal = 0;
+  let handIsSoft = false;
   const blackJackScore = {
     total: 0,
     isSoft: false
   };
 
-  const filteredAces = hand.filter(card => card.displayVal === "Ace" && card.val === 11);
-  console.log(filteredAces);
+  // check if there are aces in hand
+  let filteredAces = hand.filter((card) => (card.displayVal === "Ace") && (card.val === 11));
+  
+  console.log(`This is number of filtered Aces ${filteredAces.length}`);
+
+  // if only 1 ace
+  if(filteredAces.length == 1) {
+    handIsSoft = true;
+  }
+  // if more than 1 ace
+  else if(filteredAces.length > 1) {
+    handIsSoft = true;
+    // change ace card value to 1
+    for(let i = 1; i < filteredAces.length; i++) {
+      filteredAces[i].val = 1;
+    }
+  }
+
+  for(card of hand) {
+    handTotal += card.val;
+  }
+
+  blackJackScore.total = handTotal;
+  blackJackScore.isSoft = handIsSoft;
 
   return blackJackScore;
 }
@@ -100,7 +125,24 @@ const calcPoints = (hand) => {
  */
 const dealerShouldDraw = (dealerHand) => {
   // CREATE FUNCTION HERE
+  let total = 0;
 
+  // calc total
+  for(card of dealerHand) {
+    total += card.val;
+  }
+  // if dealer less than 16 points, should draw
+  if(total < 16) {
+    return true;
+  }
+  // if dealer equal 17 points, check for ace
+  if(total == 17) {
+
+    return true;
+  }
+
+  // by default: dealer higher than 17, hold and no draw
+  return false;
 }
 
 /**
@@ -110,8 +152,7 @@ const dealerShouldDraw = (dealerHand) => {
  * @returns {string} Shows the player's score, the dealer's score, and who wins
  */
 const determineWinner = (playerScore, dealerScore) => {
-  // CREATE FUNCTION HERE
-
+  return (playerScore === dealerScore ? "It's a tie." : playerScore > dealerScore ? `Winner is ${playerScore}` : `Winner is ${dealerScore}`);
 }
 
 /**
@@ -140,14 +181,27 @@ const startGame = function() {
   dealer.drawCard();
   player.drawCard();
   dealer.drawCard();
+  
+  // to allow program to work in node.js
+  const readline = require("readline");
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+
 
   let playerScore = calcPoints(player.hand).total;
   showHand(player);
-  while (playerScore < 21 && confirm(getMessage(playerScore, dealer.hand[0]))) {
+  while (playerScore < 21 && rl.question(getMessage(playerScore, dealer.hand[0]))) {
     player.drawCard();
     playerScore = calcPoints(player.hand).total;
     showHand(player);
   }
+  // while (playerScore < 21 && confirm(getMessage(playerScore, dealer.hand[0]))) {
+  //   player.drawCard();
+  //   playerScore = calcPoints(player.hand).total;
+  //   showHand(player);
+  // }
   if (playerScore > 21) {
     return 'You went over 21 - you lose!';
   }
@@ -166,4 +220,4 @@ const startGame = function() {
 
   return determineWinner(playerScore, dealerScore);
 }
-// console.log(startGame());
+console.log(startGame());
